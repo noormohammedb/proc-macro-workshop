983a16f128fdd7c3e4b64128761b5f3c973c76a2
Noor Mohammed <noormohammedb@protonmail.com>
2023-03-24 14:58:58 +0400

    fix first test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,13 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    eprintln!("{:#?}", ast);
 
-    unimplemented!()
+    // unimplemented!()
+
+    TokenStream::new()
 }
