c67dc81b122f451e082c67763ae06f06a0cab07c
Noor Mohammed B <noormohammedb@protonmail.com>
2023-03-26 01:19:22 +0400

    fix second test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,24 @@
+extern crate proc_macro;
+
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    // let _ = input;
     let ast = parse_macro_input!(input as DeriveInput);
     eprintln!("{:#?}", ast);
-
-    // unimplemented!()
-
-    TokenStream::new()
+    let name = &ast.ident;
+    let builder_name = format!("{}Builder", name);
+    let builder_ident = syn::Ident::new(&builder_name, name.span());
+    let expanded = quote! {
+    struct #builder_ident {
+    }
+    impl #name {
+         fn builder() -> #builder_ident {
+            #builder_ident{
+            }
+        }
+    }};
+    expanded.into()
 }
