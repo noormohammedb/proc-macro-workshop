221882fa399d73634b7431542213be95b6bdc5e3
Noor Mohammed B <noormohammedb@protonmail.com>
2023-03-26 16:47:57 +0400

    completed first 2 tests in builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -7,18 +7,27 @@ use syn::{parse_macro_input, DeriveInput};
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    eprintln!("{:#?}", ast);
     let name = &ast.ident;
     let builder_name = format!("{}Builder", name);
     let builder_ident = syn::Ident::new(&builder_name, name.span());
-    let expanded = quote! {
-    struct #builder_ident {
-    }
-    impl #name {
-         fn builder() -> #builder_ident {
-            #builder_ident{
+
+    quote!(
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl  #name {
+            pub  fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
             }
         }
-    }};
-    expanded.into()
+    )
+    .into()
 }
