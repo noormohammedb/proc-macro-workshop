827fbffae62dd9994ffaf379342ce252a3291c6f
Noor Mohammed <noormohammedb@protonmail.com>
2023-03-28 16:13:28 +0400

    completed builders 5 test and generalized the macro

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -7,40 +7,82 @@ use syn::{parse_macro_input, DeriveInput};
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
+    // eprintln!("{:#?}", ast);
     let name = &ast.ident;
     let builder_name = format!("{}Builder", name);
     let builder_ident = syn::Ident::new(&builder_name, name.span());
 
+    let fields = match &ast.data {
+        syn::Data::Struct(syn::DataStruct { fields, .. }) => match fields {
+            syn::Fields::Named(syn::FieldsNamed { named, .. }) => named,
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    };
+
+    // let named_fields = match ast.data {
+    //     syn::Data::Struct(syn::DataStruct {
+    //         fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+    //         ..
+    //     }) => named,
+    //     _ => todo!(),
+    // };
+
+    // let fields = match ast.data {
+    //     syn::Data::Struct(syn::DataStruct { fields, .. }) => match fields {
+    //         syn::Fields::Named(syn::FieldsNamed { ref named, .. }) => named,
+    //         _ => unimplemented!(),
+    //     },
+    //     _ => unimplemented!(),
+    // };
+
+    let fields_iter = fields.iter();
+
+    let optionized = fields.iter().map(|fld| {
+        let name = &fld.ident;
+        let ty = &fld.ty;
+        quote! { #name: std::option::Option<#ty> }
+    });
+
+    let methods = fields.iter().map(|fld| {
+        let name = &fld.ident;
+        let ty = &fld.ty;
+        quote! {
+            fn #name(&mut self, #name: #ty) -> &mut Self{
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
+    let build_fields = fields.iter().map(|fld| {
+        let name = &fld.ident;
+        quote! {
+            #name: self.#name.clone().ok_or(concat!(stringify!(#name), "is not set"))?
+        }
+    });
+
+    let build_empty = fields.iter().map(|fld| {
+        let name = &fld.ident;
+        quote! { #name: None }
+    });
+
     quote!(
         pub struct #builder_ident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            // #(#fields_iter,)*
+            #(#optionized,)*
         }
         impl #builder_ident {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-            fn args(&mut self, arg: Vec<String>) -> &mut Self {
-                self.args = Some(arg);
-                self
-            }
-            fn env(&mut self, envs: Vec<String>) -> &mut Self {
-                self.env = Some(envs);
-                self
-            }
-            fn current_dir(&mut self, dir: String) -> &mut Self {
-                self.current_dir = Some(dir);
-                self
-            }
-            pub fn build(&mut self) -> Result<Command, Box<dyn std::error::Error>> {
+            #(#methods)*
+
+            pub fn build(&self) -> Result<Command, Box<dyn std::error::Error>> {
                 Ok(#name {
-                    executable: self.executable.clone().ok_or("executable not found")?,
-                    args: self.args.clone().ok_or("args not found")?,
-                    env: self.env.clone().ok_or("env not found")?,
-                    current_dir: self.current_dir.clone().ok_or("current_dir not found")?,
+                    #(#build_fields,)*
+
+                    // executable: self.executable.clone().ok_or("executable not found")?,
+                    // args: self.args.clone().ok_or("args not found")?,
+                    // env: self.env.clone().ok_or("env not found")?,
+                    // current_dir: self.current_dir.clone().ok_or("current_dir not found")?,
                 })
 
             }
@@ -48,10 +90,11 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #name {
             pub  fn builder() -> #builder_ident {
                 #builder_ident {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#build_empty,)*
+                    // executable: None,
+                    // args: None,
+                    // env: None,
+                    // current_dir: None,
                 }
             }
         }
